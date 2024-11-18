a6681b28048d82e4d581d9d669899754ecfad0ba
mieramensatu <sasakihaise985@gmail.com>
2024-11-18 14:24:46 +0700

    add home page components and styles for hero, destinations, and partners

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,10 +2,14 @@ import loadComponent from "../../helpers/loadComponent.js";
 import { smoothScroll } from "../../helpers/smoothScroll.js";
 import { navbar } from "../../components/navbar/navbar.js";
 import { url } from "../../helpers/urlConfig.js";
+import slide from "./partner/slide.js";
 
 export async function main() {
   const promises = [
     loadComponent("header.navbar", url.components.navbar + "navbar.html"),
+    loadComponent(".home .hero", url.pages.home + "hero/hero.html"),
+    loadComponent(".home .destinasi", url.pages.home + "topdestinasi/topdestinasi.html"),
+    loadComponent(".home .partner", url.pages.home + "partner/partner.html"),
     loadComponent("footer.footer", url.components.footer + "footer.html"),
   ];
 
@@ -13,6 +17,7 @@ export async function main() {
     .then(() => {
       smoothScroll();
       navbar();
+      slide();
     })
     .catch((error) => {
       console.error("Error loading components:", error);
