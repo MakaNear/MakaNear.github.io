617f39b038ff6b9069424befef39bf78332503fd
mieramensatu <sasakihaise985@gmail.com>
2024-11-23 10:17:54 +0700

    add new sections for partners, information, and FAQ; update home page structure and styles

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,6 +3,7 @@ import { smoothScroll } from "../../helpers/smoothScroll.js";
 import { navbar } from "../../components/navbar/navbar.js";
 import { url } from "../../helpers/urlConfig.js";
 import slide from "./partner/slide.js";
+import Faq from "./faq/faq.js";
 
 export async function main() {
   const promises = [
@@ -10,7 +11,9 @@ export async function main() {
     loadComponent(".home .hero", url.pages.home + "hero/hero.html"),
     loadComponent(".home .benefit", url.pages.home + "benefit/benefit.html"),
     loadComponent(".home .partner", url.pages.home + "partner/partner.html"),
-    loadComponent(".home .kontak", url.pages.home + "kontak/kontak.html"),
+    loadComponent(".home .tim", url.pages.home + "tim/tim.html"),
+    loadComponent(".home .information", url.pages.home + "information/information.html"),
+    loadComponent(".home .faq", url.pages.home + "faq/faq.html"),
     loadComponent("footer.footer", url.components.footer + "footer.html"),
   ];
 
@@ -19,6 +22,7 @@ export async function main() {
       smoothScroll();
       navbar();
       slide();
+      Faq();
     })
     .catch((error) => {
       console.error("Error loading components:", error);
--- src/pages/home/partner/slide.js
+++ src/pages/home/partner/slide.js
@@ -1,10 +1,11 @@
 export default function slide() {
-    const logoSlide = document.querySelector(".scrolling-logos .logo-container .logo-slide");
-    if (logoSlide) {
-      const clone = logoSlide.cloneNode(true);
-      logoSlide.parentNode.appendChild(clone);
-    } else {
-      console.error("Element '.logo-slide' not found.");
-    }
+  const logoSlide = document.querySelector(
+    ".scrolling-logos .logo-container .logo-slide"
+  );
+  if (logoSlide) {
+    const clone = logoSlide.cloneNode(true);
+    logoSlide.parentNode.appendChild(clone);
+  } else {
+    console.error("Element '.logo-slide' not found.");
   }
-  
+}
